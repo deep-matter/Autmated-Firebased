ce507162835466374dbd084adc5222e80e46be77
Youness <youness.elbrag@aigot.com>
2024-01-11 02:31:39 +0100

    fixed Quota Fails wthi Quee Algo

--- core/Sender.py
+++ core/Sender.py
@@ -1,14 +1,19 @@
 from typing import Optional
+from numpy import ndarray
+import pandas as pd
+from tqdm import tqdm
+import time
+
+from typing import Optional
 import pandas as pd
 from tqdm import tqdm
 import time
 
 class Sender:
     def __init__(self, data_csv_path: str, authentication):
-        super(Sender, self).__init__()
-
         self.data_csv_path = data_csv_path
         self.authentication = authentication
+        self.queues = [[] for _ in range(5)]
 
     def send_password_reset_email(self) -> None:
         """
@@ -23,14 +28,41 @@ class Sender:
         # Iterate over the rows in the CSV file and send password reset emails
         for index, row in tqdm(user_data.iterrows(), total=len(user_data), desc="Processing Sending Password Reset Emails"):
             email = row['Email']
-
+            time.sleep(1)
             try:
                 # Send password reset email
                 self.authentication.send_password_reset_email(email)
                 tqdm.write(f"Password reset email sent successfully to {email}")
-                time.sleep(0.05)
+
             except Exception as e:
-                tqdm.write(f"Error sending password reset email to {email}: {e}")
+                # Handle the error if the email quota is exceeded
+                error_message = str(e)
+                if "QUOTA_EXCEEDED" in error_message:
+                    self.queues[0].append(email)
+                    tqdm.write(f"Email queued for sending later: {email}")
+                    time.sleep(2)
+
+        self.process_queued_emails()
+
+    def process_queued_emails(self) -> None:
+        for i, queue in enumerate(self.queues):
+            if queue:
+                tqdm.write(f"Start Queued {i+1} Emails left in the database to be sent")
+                for email in queue:
+                    try:
+                        self.authentication.send_password_reset_email(email)
+                        tqdm.write(f"Password reset email sent successfully from queue to {email}")
+                    except Exception as e:
+                        error_message = str(e)
+                        if "QUOTA_EXCEEDED" in error_message:
+                            self.queues[(i+1) % 5].append(email)
+                            tqdm.write(f"Email queued for sending later: {email}")
+                            time.sleep(2)
+                time.sleep(1)
+                tqdm.write(f"Freze Queue {i+2} the Auth for Period of time")
+                time.sleep(1)
+                
+        print(f"Done Queued Emails sent to the database")
 
     def send_verification_email_user(self) -> None:
         """
@@ -90,8 +122,8 @@ class Settings(Sender):
 
     def sending(self, method):
         if method == "reset":
-            return self.send_password_reset_email()
+            self.send_password_reset_email()
         elif method == "verify":
-            return self.send_verification_email_user()
+            self.send_verification_email_user()
         elif method == "change":
-            return self.change_email_address()
+            self.change_email_address()
